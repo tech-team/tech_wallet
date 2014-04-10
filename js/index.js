9f60213e49436bfb9b9937062c7732e69b7cd59c
alekseyl1992 <alekseyl@list.ru>
2014-04-10 21:49:24 +0400

    Initial upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   $(".btn-primary").click(function() {
+        window.location.href = "podtverzhdenie.html";
+        return false;
+    }) ;
+
+    $(".btn-ok").click(function() {
+        window.location.href = "status.html";
+        return false;
+    }) ;
+});
